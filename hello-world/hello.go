47ebd2eeea9b9cd717fcd6336008dc994a9bf514
Breno Jacubovski <brenojacubovski@gmail.com>
2020-05-29 15:48:32 -0300

    feat: Exemple aboute control flow

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -2,6 +2,27 @@ package main
 
 import "fmt"
 
+// control flow:
+// (1) sequence
+// (2) loop; iterative
+// (3) conditional
+
 func main() {
 	fmt.Println("Hello")
+	foo()
+	fmt.Println("something more")
+	for i := 0; i <= 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+	}
+	bar()
+}
+
+func foo() {
+	fmt.Println("world")
+}
+
+func bar() {
+	fmt.Println("and then we exited")
 }
